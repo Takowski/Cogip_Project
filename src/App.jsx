5aae7f8780891563a6f71f82b944065ba171ccfe
Takowski <93830775+Takowski@users.noreply.github.com>
2023-12-04 16:48:03 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
-
-function App() {
+const App = () => {
   return (
     <>
       <div>
         <h1 className="text-3xl text-red-600 font-bold underline">
           Hello world!
         </h1>
+        <h2>Hello from the main page of the app!</h2>
+        <p>Here are some examples of links to other pages</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="profile">Profile page</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
+import Profile from './profile.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
