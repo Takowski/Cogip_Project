d8d07236a97858268955ad1607164b4d4775f8f0
Justin Michel <justinmichel2000@gmail.com>
2023-12-13 15:31:40 +0100

    form test

---
+++ src/FormCompanies.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+  return (
+    <form className="">
+      
+      <input
+        type="text"
+        placeholder="Name"
+        className=""
+      />
+
+      <input
+        type="text"
+        placeholder="TVA"
+        className=""
+      />
+
+      <input
+        type="text"
+        placeholder="Country"
+        className=""
+      />
+
+      <input
+        type="submit"
+        value="Save"
+        className=""
+      />
+
+    </form>
+  );
---
+++ src/FormInvoices.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+  return (
+    <form className="">
+      
+      <input
+        type="text"
+        placeholder="Reference"
+        className=""
+      />
+
+      <input
+        type="number"
+        placeholder="Price"
+        className=""
+      />
+
+      <input
+        type="text"
+        placeholder="Company Name"
+        className=""
+      />
+
+      <input
+        type="submit"
+        value="Save"
+        className=""
+      />
+
+    </form>
+  );
