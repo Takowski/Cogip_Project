7d5971f5158fd59e84aa88fe2e0c04ec92feb8f2
Justin Michel <justinmichel2000@gmail.com>
2023-12-14 11:59:49 +0100

    flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import DashboardV2 from './dashboardV2'
+// import DashboardV2 from './dashboardV2'
 import FormCompanies from './FormCompanies'
 import FormContacts from './FormContacts'
 import FormInvoices from './FormInvoices'
-import Dashboard from './dashboard'
+// import Dashboard from './dashboard'
 
 
 
 function App() {
   return (
     <>
-      <DashboardV2/>
+      {/* <DashboardV2/> */}
       <FormCompanies/>
       <FormContacts/>
       <FormInvoices/>
