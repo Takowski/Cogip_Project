b48caceb0c7b4f6da9e195141c4c11339f28db43
Takowski <93830775+Takowski@users.noreply.github.com>
2023-12-19 14:36:14 +0100

    formcompany ok

--- src/FormCompanies.jsx
+++ src/FormCompanies.jsx
@@ -1,29 +1,70 @@
-import React from "react";
+import axios from 'axios';
+import React, { useState } from 'react';
 
- const countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia-Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-const type_company = ["Client","Supplier"];
+const FormCompanies = () => {
+  const [name, setName] = useState("");
+  const [tva, setTva] = useState("");
+  const [country, setCountry] = useState("");
+  const [type, setType] = useState("");
 
-function FormCompanies() {
+  const countries = ['Belgium', 'UK', 'France', 'Germany', 'Spain'];
+  const types = ['Client', 'Supplier'];
+
+  const submitForm = async (formData) => {
+    try {
+      const response = await axios.post('https://api-cogip-329f9c72c66d.herokuapp.com/api/add-company', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+
+    } catch (error) {
+      console.error(error);
+
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!name || !tva || !country || !type) {
+      console.error('All fields are required');
+      return;
+    }
+
+    const formData = {
+      company_name: name,
+      tva,
+      country,
+      type_name: type,
+    };
+
+    submitForm(formData);
+  };
+
   return (
-    <form className="mt-2 mb-2 ml-2 mr-2">
-
-      <p className="mt-4 mb-2 text-lg font-bold text-left">New Company</p>
-
-      <p className="mb-6 border"></p>
+    <form onSubmit={handleSubmit} className="mt-2 mb-2 ml-2 mr-2">
 
       <input
         type="text"
         placeholder="Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
         className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
       />
 
       <input
         type="text"
         placeholder="TVA"
+        value={tva}
+        onChange={e => setTva(e.target.value)}
         className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
       />
 
       <select
+        value={country}
+        onChange={e => setCountry(e.target.value)}
         className="mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
       >
         <option value="">Select a country</option>
@@ -34,20 +75,19 @@ function FormCompanies() {
         ))}
       </select>
 
-      <select className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
-      <option value="">Select a type</option>
-        {type_company.map((type, index_) => (
-          <option key={index_} value={type}>
+      <select
+        value={type}
+        onChange={e => setType(e.target.value)}
+        className="mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+      >
+        <option value="">Select a type</option>
+        {types.map((type, index) => (
+          <option key={index} value={type}>
             {type}
           </option>
         ))}
       </select>
-
-      <input
-        type="submit"
-        value="Save"
-        className="shadow-sm bg-violet-300 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-      />
+      <button type="submit">Submit</button>
     </form>
   );
 }
