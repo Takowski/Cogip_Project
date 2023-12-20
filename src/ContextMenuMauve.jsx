33fc9d287e4fbafa28317d9b910cfccf471b3255
Takowski <93830775+Takowski@users.noreply.github.com>
2023-12-20 13:40:23 +0100

    modal ok

--- src/ContextMenuMauve.jsx
+++ src/ContextMenuMauve.jsx
@@ -2,6 +2,8 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 const ContextMenu = ({ onEdit, onDelete }) => (
     <Popup
@@ -11,12 +13,12 @@ const ContextMenu = ({ onEdit, onDelete }) => (
         closeOnDocumentClick
         mouseLeaveDelay={300}
         mouseEnterDelay={0}
-        contentStyle={{ padding: '0px', border: 'none' }}
+        contentStyle={{ padding: '0,2rem', border: '0px', backgroundColor: '#949CD6', color: 'white', width: '100px' }}
         arrow={false}
     >
         <div className="menu">
-            <div className="menu-item" onClick={onEdit}>Edit</div>
-            <div className="menu-item" onClick={onDelete}>Delete</div>
+            <div className="menu-item" onClick={onEdit}><FontAwesomeIcon icon={faTrash}/><span> Edit</span></div>
+            <div className="menu-item" onClick={onDelete}><FontAwesomeIcon icon={faPenToSquare} /><span> Delete</span></div>
         </div>
     </Popup>
 );
