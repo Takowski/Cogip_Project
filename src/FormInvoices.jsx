dd63a2df9797f97b9014bdd9ae76192cd89ab2b0
Takowski <93830775+Takowski@users.noreply.github.com>
2023-12-20 09:18:04 +0100

    new invoice done

--- src/FormInvoices.jsx
+++ src/FormInvoices.jsx
@@ -1,22 +1,54 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 function FormInvoices() {
+  const [ref, setRef] = useState("");
+  const [dateDue, setDateDue] = useState("");
   const [companyName, setCompanyName] = useState("");
-  const [companyList, setCompanyList] = useState([]);
+  const [companies, setCompanies] = useState([]);
 
-  const handleCompanyNameChange = (event) => {
-    setCompanyName(event.target.value);
-  };
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const response = await axios.get('https://api-cogip-329f9c72c66d.herokuapp.com/api/companies');
+        setCompanies(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  const handleSubmit = (event) => {
+    fetchCompanies();
+  }, []);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    setCompanyList([...companyList, companyName]);
+
+    const formData = {
+      ref,
+      date_due: dateDue,
+      company_name: companyName,
+    };
+
+    try {
+      const response = await axios.post('https://api-cogip-329f9c72c66d.herokuapp.com/api/add-invoice', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      // Handle success here
+    } catch (error) {
+      console.error(error);
+      // Handle error here
+    }
+
+    setRef("");
+    setDateDue("");
     setCompanyName("");
   };
 
   return (
-    <form className="mt-2 mb-2 ml-2 mr-2 ">
+    <form className="mt-2 mb-2 ml-2 mr-2 " onSubmit={handleSubmit}>
 
       <p className="mt-4 mb-2 text-lg font-bold text-left">New Invoice</p>
 
@@ -25,40 +57,35 @@ function FormInvoices() {
       <input
         type="text"
         placeholder="Reference"
+        value={ref}
+        onChange={(e) => setRef(e.target.value)}
         className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
       />
 
       <input
-        type="number"
-        placeholder="Price"
-        className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-      />
-
-<input
-        type="text"
-        value={companyName}
-        onChange={handleCompanyNameChange}
-        placeholder="Company Name"
-        className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-      />
-
-      
-
-        <input
         type="date"
         placeholder="Due Date"
-        className="mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-        />
-   
-      <input
-        type="submit"
-        value="Save"
-        className="shadow-sm bg-violet-300 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+        value={dateDue}
+        onChange={(e) => setDateDue(e.target.value)}
+        className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
       />
+
+      <select
+        value={companyName}
+        onChange={(e) => setCompanyName(e.target.value)}
+        className=" mb-6 shadow-sm bg-neutral-100 border-gray-300 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+      >
+        {Array.isArray(companies) && companies.map((company) => (
+          <option key={company.id} value={company.company_name}>
+            {company.company_name}
+          </option>
+        ))}
+      </select>
+
+      <button type="submit">Submit</button>
 
     </form>
   );
 }
 
 export default FormInvoices;
-
